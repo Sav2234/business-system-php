cab539be683a0330914a38e0a8c55b7858d7dad9
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2022-01-21 14:24:38 -0500

    signup page basic functionality working

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,9 +1,17 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    echo "works"
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"]
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
 }
 
 else {
-    header("location: ../signup.php")
+    header("location: ../signup.php");
 }

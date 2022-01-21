cc48a6bf7cd1915249b35396f144a32251a7ca37
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2022-01-21 15:05:39 -0500

    signup page/working on database funcionality

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -47,5 +47,23 @@ function pwdMatch($pwd, $pwdRepeat) {
 function uidExists($conn, $username, $email) {
     $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
     $stmt = mysqli_stmt_init($conn);
-    
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();  
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if($row = mysqli_fetch_assoc($resultData)) {
+        return $row;
+    }
+
+    else {
+        $result = false;
+        return $result;
+    }
+
 }

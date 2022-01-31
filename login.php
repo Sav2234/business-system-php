3b72e412fc8f3a1a2c95ea80ae6547491187faac
Savion <s.e.williams2001@gmail.com>
2022-01-31 03:48:27 -0500

    fixed login errors

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
         <h2>Login</h2>
         <div class="login-form-form">
             <form action="includes/login.inc.php" method="post">
-                <input type="text" name="name" placeholder="Username/Email...">
+                <input type="text" name="uid" placeholder="Username/Email...">
                 <input type="password" name="pwd" placeholder="Password...">
                 <input type="password" name="pwdrepeat" placeholder="Reenter Password...">
                 <button type="submit" name="submit">Login</button>
